3aca8e7bcebadf9e1397ad815485eb07ce415b6b
ykwizera <144004543+ykwizera@users.noreply.github.com>
2024-11-25 16:09:24 +0200

    mindwave

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from app import create_app, db
 from app.models import User
 
-# Create an app instance
 app = create_app()
 
-# Create the database tables
 with app.app_context():
     db.create_all()
 
